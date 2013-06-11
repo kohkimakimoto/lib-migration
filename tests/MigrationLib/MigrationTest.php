3fa5ac445152cbd56d534e177e442f73069516bc
kohkimakimoto <kohki.makimoto@gmail.com>
2013-06-11 17:11:27 +0900

    Update to adjust Travis CI MySQL environment.

--- tests/MigrationLib/MigrationTest.php
+++ tests/MigrationLib/MigrationTest.php
@@ -7,12 +7,14 @@ class MigrationTest extends \PHPUnit_Framework_TestCase
 {
   public function testStatus()
   {
+    // Database Settings adjusts Travis CI environment.
+    // http://about.travis-ci.org/docs/user/database-setup/
     $migration = new Migration(array(
       'databases' => array(
         'yourdatabase' => array(
-          'database_dsn'      => 'mysql:dbname=yourdatabase;host=localhost',
-          'database_user'     => 'user',
-          'database_password' => 'password',
+          'database_dsn'      => 'mysql:dbname=myapp_test;host=127.0.0.1',
+          'database_user'     => 'root',
+          'database_password' => '',
           'schema_version_table' => 'schema_version'
         ),
       ),
