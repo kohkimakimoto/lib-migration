40d034f211c123ff01295fe80be8d743513c3834
kohkimakimoto <kohki.makimoto@gmail.com>
2013-06-17 11:51:38 +0900

    Update tests

--- tests/prepare_tests.sh
+++ tests/prepare_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mysql -uroot -p -e 'create database migration_lib_test;'
-mysql -uroot -p -e "GRANT ALL PRIVILEGES ON *.* TO test_user@'127.0.0.1' IDENTIFIED BY 'test_user';"
+mysql -uroot -e 'create database migration_lib_test;'
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO test_user@'127.0.0.1' IDENTIFIED BY 'test_user';"
 
