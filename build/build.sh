e224a811bd9721bc77ed6b79cd2059f779f5405d
kohkimakimoto <kohki.makimoto@gmail.com>
2013-06-24 15:18:27 +0900

    Add archiving script.

---
+++ build/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PHP=`which php`
+GIT=`which git`
+
+DIR=`$PHP -r "echo dirname(realpath('$0'));"`
+
+if [ ! -d "$DIR/lib-migration" ]; then
+    mkdir -p $DIR/lib-migration
+fi
+
+cd $DIR/lib-migration
+if [ ! -f "composer.phar" ]; then
+    curl -s http://getcomposer.org/installer 2>/dev/null | $PHP >/dev/null 2>/dev/null
+else
+    $PHP composer.phar self-update >/dev/null 2>/dev/null
+fi
+
+
+cp $DIR/skeleton/composer.json composer.json
+cp -pr $DIR/skeleton/databases databases
+
+$PHP composer.phar install -q
+
+rm composer.phar
+
+cd $DIR
+tar zcpf "$DIR/lib-migration.tgz" lib-migration
+
+rm -rf lib-migration
